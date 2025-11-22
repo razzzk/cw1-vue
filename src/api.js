952d88dbe6a3e5b18992e423c0fbf94fdd17292f
razzzk <82186776+razzzk@users.noreply.github.com>
2025-11-22 22:52:22 +0000

    Add files via upload

--- src/api.js
+++ src/api.js
@@ -1,6 +1,21 @@
-// src/api.js
 const API = 'http://localhost:3000'; //This has to be switched up to render.com, currently working on local for troubleshooting
 
 export function fetchLessons() {
   return fetch(`${API}/lessons`).then(r => r.json());
+}
+
+export function postOrder(payload) {
+  return fetch(`${API}/orders`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(payload),
+  }).then(r => r.json());
+}
+
+export function putLesson(id, data) {
+  return fetch(`${API}/lessons/${id}`, {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(data),
+  }).then(r => r.json());
 }

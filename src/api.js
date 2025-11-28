185bd17fd445b4a382b0c1d6b041f33c88937465
razzzk <82186776+razzzk@users.noreply.github.com>
2025-11-28 16:52:19 +0000

    Add files via upload

--- src/api.js
+++ src/api.js
@@ -1,4 +1,5 @@
-const API = 'http://localhost:3000'; //This has to be switched up to render.com, currently working on local for troubleshooting
+// src/api.js
+const API = 'http://localhost:3000'; // 
 
 export function fetchLessons() {
   return fetch(`${API}/lessons`).then(r => r.json());
